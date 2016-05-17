48dcbdbb4e1fba12b97bfc59c9a05ad42eec2330
xuanguang <lvyiguang@gmail.com>
2016-05-17 14:21:19 +0800

    Ch4 is done.

--- ThinkPythonCode/Ch4/4_1_1_square.py
+++ ThinkPythonCode/Ch4/4_1_1_square.py
@@ -10,10 +10,24 @@ def polygon(t, length, n):
 	
 def circle(t, r):
 	n = 200
-	lenth = 2*3.1415927*r/n
+	lenth = 2*math.pi*r/n
 	polygon(t, lenth, n)
+    
+def polygon_arc(t, length, n, angle):
+	#t=turtle.Turtle()
+    n1 = round((angle/360)*n)
+    for i in range(n1):
+        t.fd(length)
+        t.lt(360/n)
+    turtle.mainloop()
+    
+def arc(t, r, angle):
+    n = 200
+    lenth = 2*3.1415927*r/n
+    polygon_arc(t, lenth, n, angle)
 	
 bob = turtle.Turtle()
 #polygon(bob, 50, 15)
-circle(bob, 40)
+#circle(bob, 40)
+arc(bob, 40, 270)
 
