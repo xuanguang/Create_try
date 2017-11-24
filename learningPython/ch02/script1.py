7519cbfa7da1c6dcb20378145cd4a8d67960638d
LvYiguang (Joshua) <yiguang.lv@hpe.com>
2017-11-24 11:48:02 +0800

    Added .gitignore. + test python files.

---
+++ learningPython/ch02/script1.py
@@ -0,0 +1,6 @@
+# A first python script
+import sys                     # Load a library module
+print(sys.platform)
+print(2 ** 100)
+x = 'Spam!'
+print(x * 8)                   # String repetition
