716dcc7123f3162a2653431b822519bfaac20372
LvYiguang (Joshua) <yiguang.lv@hpe.com>
2017-12-14 20:28:12 +0800

    +

--- ThinkPythonCode/Ch4/exercise/mypolygon.py
+++ ThinkPythonCode/Ch4/exercise/mypolygon.py
@@ -1,9 +1,9 @@
 import turtle
 bob = turtle.Turtle()
-#print(bob)
-#bob.fd(100)
-#bob.lt(30)
-#bob.fd(100)
+print(bob)
+bob.fd(100)
+bob.lt(30)
+bob.fd(100)
 
 '''
 bob.fd(100)
@@ -18,8 +18,8 @@ bob.lt(90)
 bob.fd(100)
 '''
 
-for i in range(4):    
-	bob.fd(100)    
-	bob.lt(90)
+# for i in range(4):    
+# 	bob.fd(100)    
+# 	bob.lt(90)
 
 turtle.mainloop()
