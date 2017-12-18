fe441be5c34358f59bfc099717e561d415e4b7e7
LvYiguang (Joshua) <yiguang.lv@hpe.com>
2017-12-18 23:27:31 +0800

    +

--- ThinkPythonCode/Ch5/5_1_1.py
+++ ThinkPythonCode/Ch5/5_1_1.py
@@ -3,10 +3,15 @@ import time
 t = time.time()
 #print(time.time())
 
-days = t // (3600*24)
-years = days //365
-seconds = t % (3600*24)
-h = seconds // 3600
-m = (seconds - h*3600) // 60
-s = seconds % 60
-print("Day:Year:Hour:Min:Sec\n",int(days), ":",int(years),":", int(h), ":", int(m), ":", int(s))
+pastDays = t // (3600*24)
+# years = pastDays
+# //365       # wrong here! because not all the years include 365 pastDays
+#!!!
+secondsInlastDay = t % (3600*24)
+h = secondsInlastDay // 3600
+# m = (secondsInlastDay - h*3600) // 60
+secondsInlastHour = secondsInlastDay % 3600
+m = secondsInlastHour // 60
+s = secondsInlastHour % 60
+# print("Day:Year:Hour:Min:Sec\n",int(pastDays), ":", int(h), ":", int(m), ":", int(s))
+print("Past days (since 1 January 1970): {0}\nHour: {1}\nMin: {2}\nSec: {3}\n".format(int(pastDays), int(h), int(m), int(s)))
