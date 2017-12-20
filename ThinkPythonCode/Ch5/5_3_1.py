010173e22e9275a2ada67c609a55437db7118968
LvYiguang (Joshua) <yiguang.lv@hpe.com>
2017-12-20 23:18:14 +0800

    +

---
+++ ThinkPythonCode/Ch5/5_3_1.py
@@ -0,0 +1,22 @@
+def is_triangle(a, b, c):
+    if (a <= 0) or (b <= 0) or (c <= 0):
+        print("No")
+    # min_stick = min(a,b,c)
+    sticks = sorted([a, b, c])
+    if (sticks[0] + sticks[1] > sticks[2]):
+        print('Yes')
+    else:
+        print('No')
+
+
+def handle_input():
+    a = input('Please input the stick - a: ')
+    a = int(a)
+    b = input('Please input the stick - b: ')
+    b = int(b)
+    c = input('Please input the stick - c: ')
+    c = int(c)
+    is_triangle(a, b, c)
+
+
+handle_input()
